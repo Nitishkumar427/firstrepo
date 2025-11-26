f563182e96fb0f0a954fe680ff45eb91ccdc5bd8
Nitish Kumar <Nitishkumar42622@gmail.com>
2025-11-26 18:17:55 +0530

    initial commit

---
+++ src/com/nitish/Demo.java
@@ -0,0 +1,12 @@
+package com.nitish;
+
+public class Demo {
+    public static void main(String[] args) {
+        System.out.println("Start debug test"); // breakpoint here
+
+        // simple loop so you understand debugging
+        for (int i = 1; i <= 5; i++) {
+            System.out.println("i = " + i);
+        }
+    }
+}
